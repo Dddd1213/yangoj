0f4591327c5c40e74ac4289426bf40b746d4254b
xiaoyangii_ <xiaoyangii@qq.com>
2023-12-08 16:04:31 +0800

    feat:完成用户登录、注册、登出、获取当前登录功能；定义全局异常处理，错误码，登录校验等初始化处理（解决了springinit模板中的问题）

---
+++ src/main/java/com/yangmao/yangoj/YangOjApplication.java
@@ -0,0 +1,13 @@
+package com.yangmao.yangoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangOjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YangOjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yangmao/yangoj/YangOjApplicationTests.java
@@ -0,0 +1,18 @@
+package com.yangmao.yangoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YangOjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void insertUser(){
+
+    }
+
+}
